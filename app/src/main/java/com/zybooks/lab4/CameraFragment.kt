5258dfb971c4109adf2ff37eb735998aeb45b428
Eric Tangedal <etangedal95@gmail.com>
2023-05-14 09:45:35 -0700

    Potentially removing CameraFragment.kt and fragment_camera.xml.
    Added map, needs location marker.

--- app/src/main/java/com/zybooks/lab4/CameraFragment.kt
+++ app/src/main/java/com/zybooks/lab4/CameraFragment.kt
@@ -25,8 +25,6 @@ import kotlin.math.min
 class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
-    private var photoFile: File? = null
-    private lateinit var photoImageView: ImageView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -42,10 +40,9 @@ class CameraFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        photoImageView = view.findViewById(R.id.photo)
 
         binding.cameraButton.setOnClickListener {
-            takePhotoClick()
+            //takePhotoClick()
         }
     }
 
@@ -54,64 +51,5 @@ class CameraFragment : Fragment() {
         _binding = null
     }
 
-    private fun createImageFile(): File {
 
-        // Create a unique filename
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
-        val imageFilename = "photo_$timeStamp.jpg"
-
-        // Create the file in the Pictures directory on external storage
-        val storageDir = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File(storageDir, imageFilename)
-    }
-
-    private fun takePhotoClick() {
-
-        // Create the File for saving the photo
-        val photoFile = createImageFile()
-
-        // Create a content URI to grant camera app write permission to photoFile
-        val photoUri: Uri = FileProvider.getUriForFile(requireContext(), "com.zybooks.lab4.fileprovider", photoFile)
-
-        // Start camera app
-        takePicture.launch(photoUri)
-    }
-
-    private val takePicture = registerForActivityResult(
-        ActivityResultContracts.TakePicture()
-    ) { success: Boolean ->
-        if (success) {
-            Toast.makeText(
-                requireContext(), "Saved photo", Toast.LENGTH_SHORT
-            ).show()
-        } else {
-            Toast.makeText(
-                requireContext(), "Did not save photo", Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun displayPhoto() {
-        // Get ImageView dimensions
-        val targetWidth = photoImageView.width
-        val targetHeight = photoImageView.height
-
-        // Get bitmap dimensions
-        val bmOptions = BitmapFactory.Options()
-        bmOptions.inJustDecodeBounds = true
-        BitmapFactory.decodeFile(photoFile!!.absolutePath, bmOptions)
-        val photoWidth = bmOptions.outWidth
-        val photoHeight = bmOptions.outHeight
-
-        // Determine how much to scale down the image
-        val scaleFactor = min(photoWidth / targetWidth, photoHeight / targetHeight)
-
-        // Decode the image file into a smaller bitmap that fills the ImageView
-        bmOptions.inJustDecodeBounds = false
-        bmOptions.inSampleSize = scaleFactor
-        val bitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath, bmOptions)
-
-        // Display smaller bitmap
-        photoImageView.setImageBitmap(bitmap)
-    }
 }
--- app/src/main/java/com/zybooks/lab4/MainActivity.kt
+++ app/src/main/java/com/zybooks/lab4/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.zybooks.lab4
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -9,17 +12,39 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.FileProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.zybooks.lab4.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var photoFile: File? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.MapFragment, mapFragment)
+            .commit()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,10 +56,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener {
-            if(MapFragment.getIsMapOn()) {
-                navController.navigate(R.id.action_MapFragment_to_CameraFragment)
-            }
-
+            takePhotoClick()
         }
     }
 
@@ -58,5 +80,51 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
+
+    private fun createImageFile(): File {
+
+        // Create a unique filename
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val imageFilename = "photo_$timeStamp.jpg"
+
+        // Create the file in the Pictures directory on external storage
+        val storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(storageDir, imageFilename)
+    }
+
+    private fun takePhotoClick() {
+
+        // Create the File for saving the photo
+        val photoFile = createImageFile()
+
+        // Create a content URI to grant camera app write permission to photoFile
+        val photoUri: Uri = FileProvider.getUriForFile(this, "com.zybooks.lab4.fileprovider", photoFile)
+
+        // Start camera app
+        takePicture.launch(photoUri)
+    }
+
+    private val takePicture = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { success: Boolean ->
+        if (success) {
+            Toast.makeText(
+                this, "Saved photo", Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                this, "Did not save photo", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
