7e4e463372115be43faafcbda054fc6c4f458c44
Eric Tangedal <etangedal95@gmail.com>
2023-05-14 11:50:23 -0700

    App now has: -Map with location tracking. -Ability to take pictures.
    Next steps:
    -Remove unused files.
    -Add sound to noise button.

--- app/src/main/java/com/zybooks/lab4/MainActivity.kt
+++ app/src/main/java/com/zybooks/lab4/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.zybooks.lab4
 
+import android.annotation.SuppressLint
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -16,11 +21,21 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.zybooks.lab4.databinding.ActivityMainBinding
 import java.io.File
@@ -33,18 +48,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var googleMap: GoogleMap
     private var photoFile: File? = null
+    private var client: FusedLocationProviderClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var locationCallback: LocationCallback? = null
+    private var zoomLevel = 15f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        val mapFragment = SupportMapFragment.newInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.MapFragment, mapFragment)
-            .commit()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -55,42 +69,120 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        if (hasLocationPermission()) {
+            trackLocation()
+        }
+
         binding.fab.setOnClickListener {
             takePhotoClick()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        binding.fab2.setOnClickListener {
+            playNoise()
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+//Map Functions
+    private fun trackLocation() {
+
+        // Create location request
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(3000)
+            .build()
+
+        // Create location callback
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                for (location in locationResult.locations) {
+                    updateMap(location)
+                }
+            }
+        }
+
+        client = LocationServices.getFusedLocationProviderClient(this)
     }
 
+    private fun updateMap(location: Location) {
+
+        // Get current location
+        val currentLatLng = LatLng(location.latitude,
+            location.longitude)
+
+        // Remove previous marker
+        googleMap.clear()
+
+        // Place a marker at the current location
+        val markerOptions = MarkerOptions()
+            .title("Here you are!")
+            .position(currentLatLng)
+        googleMap.addMarker(markerOptions)
+
+        // Zoom to previously saved level
+        val update: CameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, zoomLevel)
+
+        googleMap.animateCamera(update)
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Marker")
-        )
+        this.googleMap = googleMap
+
+        // Save zoom level
+        googleMap.setOnCameraMoveListener {
+            zoomLevel = googleMap.cameraPosition.zoom
+        }
+
+        // Handle marker click
+        googleMap.setOnMarkerClickListener { marker: Marker ->
+            val lineBreak = System.getProperty("line.separator")
+            Toast.makeText(this,
+                "Lat: ${marker.position.latitude} $lineBreak Long: ${marker.position.longitude}",
+                Toast.LENGTH_LONG).show()
+
+            return@setOnMarkerClickListener false
+        }
     }
 
+    override fun onPause() {
+        super.onPause()
+        client?.removeLocationUpdates(locationCallback!!)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        if (hasLocationPermission()) {
+            client?.requestLocationUpdates(
+                locationRequest!!, locationCallback!!, Looper.getMainLooper())
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+
+        // Request fine location permission if not already granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            return false
+        }
+        return true
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            trackLocation()
+        }
+    }
+
+//Camera Functions
     private fun createImageFile(): File {
 
         // Create a unique filename
@@ -126,5 +218,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 this, "Did not save photo", Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    //Menu Functions
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun playNoise() {
+
     }
 }
